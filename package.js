acd6e86947392417174123c458206b586337ea8b
TJ <tjmonsi@gmail.com>
2015-10-21 10:55:40 +0800

    Added fullbleed

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'littleq:littleq-polymer-loader',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'This is to simplify the loading of Polymer 1.0 packages using Bower',
   // URL to the Git repository containing the source code for this package.
@@ -10,16 +10,21 @@ Package.describe({
   documentation: 'README.md'
 });
 
-Package.onUse(function(api) {
+Package.onUse(function(api){
   api.versionsFrom('1.2.0.2');
   
   var packages = [
     "differential:vulcanize@2.0.1",
-    "mquandalle:bower@1.5.2_1"
+    "mquandalle:bower@1.5.2_1"    
   ]
   
   api.use(packages)
   api.imply(packages)
+
+  api.addFiles([
+    "lib/startup.js",
+    // "lib/startup.html"
+  ], "client")
 });
 
 
